5d9a662673da207de140e82b440c36b2ad54bd91
Colin Fendrick <colin.fendrick@gmail.com>
2020-11-02 05:39:16 -0500

    Remove add entry page and move that to modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import { Switch, Link, Route } from 'react-router-dom';
 
-import { AddEntry, Bracket, Vote } from './components';
+import { Bracket, Vote } from './components';
 import { ModalContainer } from './containers';
 import useLocalContext from './hooks/useLocalContext';
 
@@ -11,49 +11,38 @@ const App = () => {
 	return (
 		<>
 			<div>
-				<nav className = 'navbar navbar-expand navbar-dark bg-dark'>
-					<a href = '/tutorials' className = 'navbar-brand'>
-					Vote on the Outfits
+				<nav className='navbar navbar-expand navbar-dark bg-dark'>
+					<a href='/vote' className='navbar-brand'>
+						Vote on the Outfits
 					</a>
-					<div className = 'navbar-nav mr-auto'>
+					<div className='navbar-nav mr-auto'>
 						{currentUser ? (
 							<>
-								<li className = 'nav-item'>
-									<Link to ={'/vote'} className = 'nav-link'>
-									Go Vote!
+
+								<li className='nav-item'>
+									<Link to={'/bracket'} className='nav-link'>
+										View Bracket
 									</Link>
 								</li>
 
-								<li className = 'nav-item'>
-									<Link to = {'/add'} className = 'nav-link'>
-									Add Entry
-									</Link>
-								</li>
-
-								<li className = 'nav-item'>
-									<Link to ={'/bracket'} className = 'nav-link'>
-									View Bracket
-									</Link>
-								</li>
-
-								<li className = 'nav-item'>
-									<Link to ={'/profile'} className = 'nav-link'>
-									Profile
+								<li className='nav-item'>
+									<Link to={'/profile'} className='nav-link'>
+										Profile
 									</Link>
 								</li>
 
 							</>
 						) : (
 							<>
-								<li className = 'nav-item'>
-									<Link to ={'/login'} className = 'nav-link'>
-									Login
+								<li className='nav-item'>
+									<Link to={'/login'} className='nav-link'>
+										Login
 									</Link>
 								</li>
 
 								<li className='nav-item'>
 									<Link to={'/register'} className='nav-link'>
-                Sign Up
+										Sign Up
 									</Link>
 								</li>
 							</>
@@ -61,11 +50,10 @@ const App = () => {
 					</div>
 				</nav>
 
-				<div className = 'container mt-3' >
+				<div className='container mt-3'>
 					<Switch>
 						<Route path='/bracket' component={Bracket} />
 						<Route path='/vote' component={Vote} />
-						<Route path='/add' component={AddEntry} />
 					</Switch>
 				</div>
 			</div>
--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,9 +1,8 @@
-import AddEntry from './AddEntry';
 import Bracket from './Bracket';
 import Vote from './Vote';
 
 export {
-	AddEntry,
+
 	Bracket,
 	Vote
 };
