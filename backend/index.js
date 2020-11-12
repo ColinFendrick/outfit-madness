66372660478d989f3cff7213437a6b8a644611b3
Colin Fendrick <colin.fendrick@gmail.com>
2020-11-11 16:07:07 -0800

    Break app apart for serve

--- backend/index.js
+++ backend/index.js
@@ -47,12 +47,11 @@ app.use(userRouter);
 app.use(adminRouter);
 
 
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 // AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
 app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname + '/../build/index.html'));
+	res.sendFile(path.join(__dirname + '/../frontend/build/index.html'));
 });
 
 app.listen(PORT, () => {
